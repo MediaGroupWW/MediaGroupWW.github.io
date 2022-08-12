c328beff8351fc81396097c3890f6c1d595eeb15
MediaGroupWW <github@mediagroupww.com>
2022-08-12 10:09:44 +0200

    animations

--- script/scroll.js
+++ script/scroll.js
@@ -24,3 +24,16 @@ function parallax(e) {
     })
 }
 
+ScrollReveal().reveal('body', { duration: 800 });
+ScrollReveal().reveal('.hero', { delay: 200, distance: '200px', origin: 'left' });
+ScrollReveal().reveal('.brand-section-content', { delay: 300, distance: '200px', origin: 'left' });
+ScrollReveal().reveal('.marketing-productivity-content', { delay: 300, distance: '200px', origin: 'top', viewFactor: 0.6 });
+ScrollReveal().reveal('.marketing-productivity-hero', { delay: 300, distance: '200px', origin: 'top', viewFactor: 0.6 });
+ScrollReveal().reveal('.first-case-container', { delay: 300, distance: '200px', origin: 'left', viewFactor: 0.5 });
+ScrollReveal().reveal('.second-case-container', { delay: 300, distance: '200px', origin: 'right', viewFactor: 0.5 });
+ScrollReveal().reveal('.third-case-container', { delay: 300, distance: '200px', origin: 'left', viewFactor: 0.6 });
+ScrollReveal().reveal('.fourth-case-container', { delay: 300, distance: '200px', origin: 'right', viewFactor: 0.6 });
+ScrollReveal().reveal('.marketing-productivity-buttons', { delay: 300, distance: '200px', origin: 'bottom', viewFactor: 0.6 });
+ScrollReveal().reveal('.jobs-section', { delay: 300, distance: '200px', origin: 'left', viewFactor: 0.6 });
+
+
