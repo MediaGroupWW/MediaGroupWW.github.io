7dcafab1c233f85dc425f717de8bfd10c071a241
MediaGroupWW <github@mediagroupww.com>
2022-08-17 15:17:52 +0200

    mobile version without menu

--- script/scroll.js
+++ script/scroll.js
@@ -22,6 +22,13 @@ function parallax(e) {
         const y = (window.innerHeight - e.pageY * position)/90;
         shift.style.transform = `translateX(${x}px) translateY(${y}px)`
     })
+
+    this.querySelectorAll('#jobsSectionVector').forEach((shift) => {
+        const position = shift.getAttribute("value");
+        const x = (window.innerWidth - e.pageX * position)/90;
+        const y = (window.innerHeight - e.pageY * position)/90;
+        shift.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
 }
 
 
